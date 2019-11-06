bb2f968702bfa6d2ffeb3cb79c03ed4d020930d2
brand1345 <byuan45@gatech.edu>
2019-11-06 14:04:13 -0500

    new goal screen

--- app/src/main/java/com/example/sitbit/GoalsFragment.java
+++ app/src/main/java/com/example/sitbit/GoalsFragment.java
@@ -8,13 +8,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class GoalsFragment extends Fragment {
-
+    private SeekBar sBar;
+    private TextView tView;
 
     public GoalsFragment() {
         // Required empty public constructor
@@ -24,6 +26,9 @@ public class GoalsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+//        sBar = (SeekBar) getView().findViewById(R.id.total_time_seek);
+//        tView = (TextView) getView().findViewById(R.id.total_time_text);
+//        tView.setText(sBar.getProgress() + "/" + sBar.getMax());
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_goals, container, false);
     }
